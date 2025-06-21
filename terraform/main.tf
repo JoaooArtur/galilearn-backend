45d6621665b957c98efd47da68dcf03e67d1611a
João Belli <joaooartur.2015@gmail.com>
2025-06-21 11:05:47 -0300

    feat: testing terraform deploy

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-galilearn"
+  location = "brazilsouth"
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                = "cae-galilearn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = "galilearn-api"
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "galilearn-api"
+      image  = "acrgalilearn.azurecr.io/galilearn-api:latest"
+      cpu    = 0.5
+      memory = "1.0Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
+  }
+}
