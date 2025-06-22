9a62f66c106449e8b8caa4a5457d16a2fa7b76d1
joao.belli <joao.belli@twila.digital>
2025-06-22 15:18:39 -0300

    fix friends filter in mongo

--- src/Modules/Student/Student.Application/UseCases/Queries/ListStudentsByNameHandler.cs
+++ src/Modules/Student/Student.Application/UseCases/Queries/ListStudentsByNameHandler.cs
@@ -1,10 +1,12 @@
 ﻿using Core.Application.Messaging;
 using Core.Shared.Results;
 using MediatR;
+using MongoDB.Driver;
 using Student.Domain;
 using Student.Persistence.Projections;
 using Student.Shared.Queries;
 using Student.Shared.Response;
+using System.Linq;
 
 namespace Student.Application.UseCases.Queries
 {
@@ -13,7 +15,7 @@ namespace Student.Application.UseCases.Queries
     {
         public async Task<Result<List<StudentsByNameResponse>>> Handle(ListStudentsByNameQuery query, CancellationToken cancellationToken)
         {
-            var students = await projectionGateway.ListAsync(x => x.Name.Contains(query.Name, StringComparison.OrdinalIgnoreCase), cancellationToken);
+            var students = await projectionGateway.ListAsync(x => x.Name.ToLower().Contains(query.Name.ToLower()), cancellationToken);
 
             return Result.Success(students.Select(x => new StudentsByNameResponse(x.Id, x.Name, x.Email)).ToList());
         }
